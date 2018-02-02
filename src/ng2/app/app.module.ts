6470a8fa3d678ff9061694113135b8568e543206
Matt Huddleston <mthuddleston@gmail.com>
2018-02-01 19:47:20 -0500

    UPdating

--- src/ng2/app/app.module.ts
+++ src/ng2/app/app.module.ts
@@ -32,8 +32,4 @@ export class AppModule {
   ) {
     this.upgrade.bootstrap(document.body, ['FileManagerApp']);
   }
-  // ngDoBootstrap() { }
-  ngDoBootstrap() {
-    // this.upgrade.bootstrap(document.body, ['FileManagerApp']);
-  }
 }
